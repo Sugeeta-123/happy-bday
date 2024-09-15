2957f34ad388b63478c79c451b239000c14fd15e
Sugeeta-123 <157228044+Sugeeta-123@users.noreply.github.com>
2024-09-15 12:49:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function nextpage(){
+    var n=document.getElementById("iname").value;
+    document.getElementById("pish").innerHTML="Happy Birthday "+  n;
+    
+    var body = document.getElementsByTagName('body')[0];
+    body.style.backgroundImage = 'url(https://st2.depositphotos.com/1146092/7279/i/450/depositphotos_72790263-stock-photo-happy-birthday-dog.jpg)';
+    body.style.backgroundSize = "cover";
+}
+
+
+
+
+
+
+
+
+
+
+
